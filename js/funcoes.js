03e02824fc91c9379e565b6cb509f496e23580ac
Lucca Arantes Martins <luam@hvl.no>
2020-08-15 20:00:21 -0300

    operation working

--- js/funcoes.js
+++ js/funcoes.js
@@ -1,11 +1,15 @@
 $(document).ready(function(){
 
     var btn_Generate = $('#btn-generate');
+    var btn_sum = $('#btn-sum');
+    var btn_sub = $('#btn-sub');
+    var btn_clear = $('#btn-clear');
     var dynamic_Fields = $('#dynamic-fields');
     var btn_Calculate = $('#btn-calculate');
     // var div_Result = $('#div-result');
     var operation = $('#operation');
     var numberFields = 0;
+    var fields = [];
 
     function replicateFields(value){
         dynamic_Fields.empty();
@@ -23,10 +27,10 @@ $(document).ready(function(){
 
             newDiv.appendChild(newField);
             
-            var operator = operation.children("option:selected").val();
-            if(i != value - 1){
-                newDiv.append(operator);
-            }
+            // var operator = operation.children("option:selected").val();
+            // if(i != value - 1){
+            //     newDiv.append(operator);
+            // }
             dynamic_Fields.append(newDiv);
         }
 
@@ -38,53 +42,62 @@ $(document).ready(function(){
         replicateFields(numberFields);
     })
 
-    btn_Generate.click(function(){
-        $(document.getElementById('div-fields')).css({"visibility":"visible"});
-        numberFields = document.getElementById('input-numberFields').value;
-        if(!$.isNumeric(numberFields) || numberFields <= 0){
-            alert("The value must be a positive number!");
-        }
-        else{
-            replicateFields(numberFields);
-        }
-    });
-    
-    function operate(result, value){
-        if(result != null){
-            switch(operation.children("option:selected").val()){
-                case "+":
-                    return result + value;
-                case "-":
-                    return result - value;
-                case "*":
-                    return result * value;
-                case "/":
-                    return result / value;
-            }
-        }
-        else{
-            return value;
-        }
-    }
-
-    btn_Calculate.click(function(){
-        var result = null;
-        var valid = true;
+    function validateFields(){
+        fields = [];
+        var validation = true; 
         $(".field").each(function(){
             var value = parseFloat($(this).val());
             if(!$.isNumeric(value)){
-                alert("The values must be numbers!");
-                valid = false;
+                alert("Valores inválidos!");
+                validation = false;
                 return false;
             }
-            result = operate(result, value);
-        });
+            fields.push(value);
+        })
 
-        if(valid == true){
+        return validation;
+    }
+
+    btn_sum.click(function(){
+        var result = 0;
+        if(validateFields()){
+            fields.forEach(function(value, index){
+                if(index == 0)
+                    result = value;
+                else
+                    result += value;
+            })
             showResult(result);
         }
+    })
 
-        
+    btn_sub.click(function(){
+        var result = 0;
+        if(validateFields()){
+            fields.forEach(function(value, index){
+                if(index == 0)
+                    result = value; 
+                else
+                    result -= value;
+            })
+            showResult(result);
+        }
+    })
+
+    btn_clear.click(function(){
+        $(document.getElementById('div-fields')).css({"visibility": "hidden"});
+        $(document.getElementById('div-result')).css({"visibility": "hidden"});
+    })
+
+    btn_Generate.click(function(){
+        numberFields = document.getElementById('input-numberFields').value;
+        if(!$.isNumeric(numberFields) || numberFields < 2){
+            alert("O número de campos deve ser um valor numérico, inteiro, e igual ou maior que 2!");
+        }
+        else{
+            $(document.getElementById('div-fields')).css({"visibility":"visible"});
+            replicateFields(numberFields);
+        }
     });
 
     function showResult(result){
